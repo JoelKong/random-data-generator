b7499283b886677af587312372b1ee978edff100
Joel Kong <joelkong2001@gmail.com>
2023-05-03 13:50:43 +0800

    feat: added header

--- randomdata/components/Navigation.tsx
+++ randomdata/components/Navigation.tsx
@@ -1,7 +1,15 @@
 import classes from "./navigation.module.css";
 
 function Navigation() {
-  return <div>test</div>;
+  return (
+    <nav className={classes.nav}>
+      <a href="/">Home</a>
+      <p className={classes.header}>Random Data Generator</p>
+      <a href="https://github.com/JoelKong" target="_blank">
+        About Me
+      </a>
+    </nav>
+  );
 }
 
 export default Navigation;
