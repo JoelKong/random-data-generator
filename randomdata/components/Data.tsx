487f2d1c19090f1d94ffdd70ecddab8261f7725b
Joel Kong <joelkong2001@gmail.com>
2023-05-03 14:18:03 +0800

    feat: integrated with random data api

--- randomdata/components/Data.tsx
+++ randomdata/components/Data.tsx
@@ -1,10 +1,32 @@
 import classes from "./data.module.css";
+import { useState } from "react";
 
 function Data() {
-  return (
+  const [user, setUser] = useState("");
+
+  async function generateData() {
+    const data = await fetch(
+      "https://random-data-api.com/api/v2/users?size=1is_json=true",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const jsonData = await data.json();
+    console.log(jsonData);
+    setUser(jsonData);
+  }
+
+  return !user ? (
     <section className={classes.section}>
-      <button className={classes.button}>Generate Data</button>
+      <button className={classes.button} onClick={generateData}>
+        Generate Data
+      </button>
     </section>
+  ) : (
+    <div>hi</div>
   );
 }
 
