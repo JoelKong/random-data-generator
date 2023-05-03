ef6041a10e41ad1bc0edac01e98867966349a746
Joel Kong <joelkong2001@gmail.com>
2023-05-03 14:46:10 +0800

    feat: made number of generation feature

--- randomdata/components/Data.tsx
+++ randomdata/components/Data.tsx
@@ -1,8 +1,10 @@
 import classes from "./data.module.css";
 import { useState } from "react";
+import { useEffect } from "react";
 
 function Data() {
   const [user, setUser] = useState<any>("");
+  const [count, setCount] = useState<number>(0);
 
   async function generateData() {
     const data = await fetch(
@@ -19,29 +21,39 @@ function Data() {
     setUser(jsonData);
   }
 
+  useEffect(() => {
+    setCount((prev) => prev + 1);
+  }, [user]);
+
   return !user ? (
-    <section className={classes.section}>
-      <button className={classes.button} onClick={generateData}>
-        Generate Data
-      </button>
-    </section>
+    <>
+      <div className={classes.count}>{"Number of Generations: " + count}</div>
+      <section className={classes.section}>
+        <button className={classes.button} onClick={generateData}>
+          Generate Data
+        </button>
+      </section>
+    </>
   ) : (
-    <section className={classes.section2}>
-      <img
-        src={user.avatar}
-        width={200}
-        height={200}
-        alt="avatar"
-        className={classes.image}
-      />
-      <div className={classes.text}>
-        {"Name: " + user.first_name + " " + user.last_name}
-      </div>
-      <div className={classes.text}>{"Email: " + user.email}</div>
-      <button className={classes.button} onClick={generateData}>
-        Generate Data
-      </button>
-    </section>
+    <>
+      <div className={classes.count}>{"Number of Generations: " + count}</div>
+      <section className={classes.section2}>
+        <img
+          src={user.avatar}
+          width={200}
+          height={200}
+          alt="avatar"
+          className={classes.image}
+        />
+        <div className={classes.text}>
+          {"Name: " + user.first_name + " " + user.last_name}
+        </div>
+        <div className={classes.text}>{"Email: " + user.email}</div>
+        <button className={classes.button} onClick={generateData}>
+          Generate Data
+        </button>
+      </section>
+    </>
   );
 }
 
